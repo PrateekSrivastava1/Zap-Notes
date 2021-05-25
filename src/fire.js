1fe034dd1b6ab215fbb0354b09cb923bbb678172
Prateek <prateeksdr4@gmail.com>
2021-05-25 16:07:18 +0530

    hiding necessary info

--- src/fire.js
+++ src/fire.js
@@ -1,11 +1,7 @@
 import firebase from "firebase";
 var firebaseConfig = {
-    apiKey: "AIzaSyBhn4QFu6vglOf7qCs2egY1f-a1oxL5MEQ",
-    authDomain: "react-todo-webapp-6a666.firebaseapp.com",
-    projectId: "react-todo-webapp-6a666",
-    storageBucket: "react-todo-webapp-6a666.appspot.com",
-    messagingSenderId: "577720347436",
-    appId: "1:577720347436:web:c23a63df90d05f9965da89"
+    
   };
   const fire = firebase.initializeApp(firebaseConfig);
   export default fire;
+  
